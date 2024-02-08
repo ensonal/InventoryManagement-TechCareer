cfc17d916a29f1953f2b580c7869a3b63ff77974
Enes Önal <enes.onal777@gmail.com>
2024-02-08 15:23:19 +0300

    feat: complete components and setup router

--- Frontend/shop-management/src/app/app-routing.module.ts
+++ Frontend/shop-management/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeLoginComponent } from './auth/login/components/employee-login/employee-login.component';
+import { CustomerLoginComponent } from './auth/login/components/customer-login/customer-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employee', component: EmployeeLoginComponent },
+  { path: 'customer', component: CustomerLoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
